4967c91d9d29210cc61d5f2927f5a4fd24e9467b
james hamilton <jhamilton72.jh@gmail.com>
2019-11-25 19:18:15 -0700

    changes to quiz

---
+++ question.js
@@ -0,0 +1,5 @@
+let questions = [
+    ["Commonly used data types DO NOT include:","strings","booleans","alerts", "C"],
+  ["The condition in an if / else statement is enclosed within ____.","parentheses","square brackets","quotes", "C"],
+   ["It is best to use___.","var","let","const","B"],
+];
